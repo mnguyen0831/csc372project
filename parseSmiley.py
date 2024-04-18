56eb69f8604b0e3b841ba228fd3ac8bd73a9ab2a
Matthew Nguyen <mettchue@gmail.com>
2024-04-18 05:34:25 -0700

    Added a new test file, --vars option, and implemented Declare and Assign, but Assign only works with literals

--- parseSmiley.py
+++ parseSmiley.py
@@ -1,11 +1,79 @@
-def declare(line: list[str]) -> None:
+def declare(line: list[str], variables: dict[str, list[str, bool | int | str]], lnum: int) -> dict[str, list[str, bool | int | str]]:
     # <declare st> ::= <type> <var id> . | <type> <const id> _is <literal> .
-    # if line[1] starts with lowercase then variables[line[0]] = None
-    # if line[1] starts with uppercase
-    #   if line[2] == '_is'
-    #       if type of line[3] is what line[0] says 
-    #           variables[line[0]] = line[3]
-    # if line[4] != '.'
-    #   throw error
-    # else throw error
-    pass 
+    var = variables.copy()
+    if len(line) < 3:
+        if line[1][0].islower():
+            if line[2] == '_is':
+                raise Exception(f"Variable '{line[1]}' at line {lnum} cannot be assigned at declaration")
+        raise Exception(f"Declare statement syntax at line {lnum} is incorrect")
+    if line[-1] != '.':
+        raise Exception(f"Line {lnum} not terminated with '.'")
+    if line[1][0].islower():
+        var[line[1]] = [line[0], None]
+    elif line[1][0].isupper():
+        if line[2] != '_is':
+            raise Exception(f"Constant {line[1]} at line {lnum} must be assigned at declaration")
+        if len(line) != 5:
+            raise Exception(f"Declare statement syntax at line {lnum} is incorrect")
+        if line[0] == '_int':
+            try:
+                val = int(line[3])
+            except:
+                raise Exception(f"'{line[3]}' at line {lnum} is not a valid int value")
+            else:
+                var[line[1]] = ['const ' + line[0], val]
+        elif line[0] == '_bool':
+            if line[3] in {':)','(:',':^)', '(^:', ':`)', '(`:'}:
+                val = True
+            elif line[3] in {':(','):',':^(', ')^:', ':`(', ')`:'}:
+                val = False
+            else:
+                raise Exception(f"'{line[3]}' at line {lnum} is not a valid boolean value")
+            var[line[1]] = ['const ' + line[0], val]
+        else:
+            var[line[1]] = ['const ' + line[0], line[3]]
+    return var 
+
+def assign(line: list[str], variables: dict[str, list[str, bool | int | str]], lnum: int) -> dict[str, list[str, bool | int | str]]:
+    # <assign st> ::= <var id> _is <expr> . | <var id> ++ . | <var id> += <expr> .
+    var = variables.copy()
+    if len(line) < 3:
+        raise Exception(f"Assign statement syntax at line {lnum} is incorrect")
+    if line[1] == '_is':
+        if len(line[0][0]) > 5:
+            raise Exception(f"Constant {line[0]} of type {var[line[0]][0]} on line {lnum} cannot be reassigned.")
+        var[line[0]][1] = expr(var[line[0]][0], line[2:], var, lnum)
+    elif line[1] == '++':
+        if var[line[0]][0] == '_int':
+            if var[line[0]][1] is None:
+                raise Exception(f"Variable '{line[0]}' hasn't been initialized yet")
+            var[line[0]][1] += 1
+        elif var[line[0]][0] == 'const _int':
+            raise Exception(f"Constant '{line[0]}' at line {lnum} cannot be reassigned")
+        else:
+            raise Exception(f"Operation '++' at line {lnum} is invalid for {var[line[0]][0]} types")
+    elif line[1] == '+=':
+        if var[line[0]][0] == '_int':
+            if var[line[0]][1] is None:
+                raise Exception(f"Variable '{line[0]}' hasn't been initialized yet")
+            pass # var[line[0]][1] += expr(line[2:], variables)
+        elif var[line[0]][0] == 'const _int':
+            raise Exception(f"Constant '{line[0]}' at line {lnum} cannot be reassigned")
+        else:
+            raise Exception(f"Operation '+=' at line {lnum} is invalid for {var[line[0]][0]} types")
+    return var
+        
+# Pass in the type expected (_int, _str, _bool), the portion of line that is the expression, which should be the 
+# rest of the line (include the '.' token), the current variables list, and the line number lnum
+def expr(type: str, line: list[str], variables: dict[str, list[str, bool | int | str]], lnum: int) -> str | int | bool:
+    print("Entered Expression")
+    if type == '_int':
+        return int(line[0])
+    elif type == '_str':
+        return line[0]
+    elif type == '_bool':
+        if line[0] in {':)','(:',':^)', '(^:', ':`)', '(`:'}:
+            return True
+        elif line[0] in {':(','):',':^(', ')^:', ':`(', ')`:'}:
+            return False
+    return None
--- runSmiley.py
+++ runSmiley.py
@@ -6,49 +6,73 @@ input_line: str
 input_file: str
 output_file: str
 print_ast: bool
+print_vars: bool
 program: list[list[str]]
-variables: dict[str, bool | int | str]
+variables: dict[str, list[str, bool | int | str]] # key: id, val: [type, value]
+cur_line: int
 
 # Files can be run using:
 # python runSmiley.py --input-file 'sample.;]'
 # python runSmiley.py --input-line '1 + 2 + 3 .'
 
 def main() -> None:
-    global program, variables, print_ast
+    global program, variables, print_ast, print_vars, cur_line
     variables = dict()
+    cur_line = 1
     getInput()
     if input_file is None:
-        program = scanLine(input_line)
+        program = [scanLine(input_line)]
     else:
-        program = [scanSmiley(input_file)]
-    for line in program:
-        execute(line)
+        program = scanSmiley(input_file)
+    while cur_line - 1 < len(program):
+        #print(f"Executing line {cur_line}: {program[cur_line - 1]}")
+        execute(program[cur_line - 1])
     if print_ast:
         print("Print AST not implemented.")
+    if print_vars:
+        print("\nVariables:\n----------")
+        for var in variables.keys():
+            print(f"{var} : {variables[var]}")
 
 def getInput() -> None:
-    global input_line, input_file, output_file, print_ast
+    global input_line, input_file, output_file, print_ast, print_vars
     parser = argparse.ArgumentParser(description='Reads command line arguments.')
     parser.add_argument('--input-line', type=str, help='Line to run')
     parser.add_argument('--input-file', type=str, default=None, help='File name to run')
     parser.add_argument('--output-file', type=str, help='File to write to output')
     parser.add_argument('--ast', type=bool, default=False, help='Prints out Syntax Tree')
+    parser.add_argument('--vars', type=bool, default=False, help='Prints out Variable Dictionary')
     args = parser.parse_args()
     input_line = args.input_line
     input_file = args.input_file
     output_file = args.output_file
     print_ast = args.ast
+    print_vars = args.vars
 
 def execute(line: list[str]) -> None:
-    global variables
+    global variables, cur_line
+    if len(line) == 0:
+        cur_line += 1
+        return
     if line[0] in {'$'}:
+        cur_line += 1
         return
     elif line[0] in {'_int', '_str', '_bool'}:
-        smile.declare(line)
+        variables = smile.declare(line, variables, cur_line)
+        #print(f"Variables updated: {variables}")
     elif line[0] in variables.keys():
-        pass #assign(line)
+        variables = smile.assign(line, variables, cur_line)
+        #print(f"Variables updated: {variables}")
+    elif line[0] == '_read':
+        pass # variables = smile.read(line, variables, cur_line)
     elif line[0] in {'_write', '_writeline'}:
-        pass #print(line)
+        pass #smile.print(line, variables)
+    else:
+        if line[0].isnumeric() or line[0][0] == '_' or line[0][0].isupper():
+            raise Exception(f"'{line[0]}' at line {cur_line} is an invalid name for a variable")
+        else:
+            raise Exception(f"Variable '{line[0]}' at line {cur_line} hasn't been declared")
+    cur_line += 1
 
 
 if __name__ == "__main__":
